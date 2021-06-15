c60b5624db441597edce0018b0493d3362b0b752
Mary <oxesescom@gmail.com>
2021-06-14 23:28:50 -0500

    add New post

--- src/components/CustomTextarea.jsx
+++ src/components/CustomTextarea.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./customTextarea.css";
 
 export default function CustomTextarea(props) {
   return (
     <React.Fragment>
       <textarea
-        className="form-control"
+        className={`form-control ${props.style}`}
         id={props.id}
         placeholder={props.placeholder}
         value={props.value}
