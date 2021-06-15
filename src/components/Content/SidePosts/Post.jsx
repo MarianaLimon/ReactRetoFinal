30690bd08d443bff7446a3c50a45148d69cc1ccc
Beto Barrera <betomonter@gmail.com>
2021-06-14 23:17:59 -0500

    fix post

--- src/components/Content/SidePosts/Post.jsx
+++ src/components/Content/SidePosts/Post.jsx
@@ -12,145 +12,76 @@ import profile3 from "../../../images/profiles/user4.webp";
 import imgReactions from "../../../images/reaction-heart.svg";
 import imgComments from "../../../images/reaction-comment.svg";
 
-const postsData = {
-  post1: [
-    {
-      title: "Diagnosing Performance Issues",
-      articleImg: imgArticleImg,
-      author: "Maria",
-      authorImg: profile1,
-      date: "Ene 6",
-      reactions: "10",
-      comments: "5",
-      read: "2",
-    },
-  ],
-  post2: [
-    {
-      title:
-        "I landed my first dev job after 6 months of building projects. Here's my story!",
-      articleImg: imgArticleImg,
-      author: "Gustavo",
-      authorImg: profile2,
-      date: "May 8",
-      reactions: "20",
-      comments: "2",
-      read: "5",
-    },
-  ],
-  post3: [
-    {
-      title: "Vue.js vs. React — Not Your Usual Comparison",
-      articleImg: imgArticleImg,
-      author: "Domagoj Vidovic",
-      authorImg: profile3,
-      date: "Jun 7",
-      reactions: "60",
-      comments: "7",
-      read: "15",
-    },
-  ],
-  post4: [
-    {
-      title: "Post de Prueba",
-      articleImg: imgArticleImg,
-      author: "Camilo Cestso",
-      authorImg: profile3,
-      date: "Jun 45",
-      reactions: "20",
-      comments: "2",
-      read: "35",
-    },
-  ],
-};
-
-function Post() {
-  const postsUI = Object.entries(postsData)
-    .reverse()
-    .map(([type, posts], index) => (
-      <React.Fragment>
-        {posts.map(
-          (
-            {
-              title,
-              articleImg,
-              author,
-              authorImg,
-              date,
-              reactions,
-              comments,
-              read,
-            },
-            index
-          ) => (
-            <li className={`card p-0 ${Styles.CardContainer}`} key={index}>
-              {/* Imagen del Post */}
-              <div className={`${Styles.ArticleImg}`}>
-                <img className={`w-2`} src={`${articleImg}`} />
+export default function Post(props) {
+  console.log(props);
+  return (
+    <React.Fragment>
+      <div key={props.id} className={`card ${Styles.AppCard}`}>
+        <div className="card-body">
+          <li className={`card ${Styles.CardContainer}`}>
+            {/* Imagen del Post */}
+            <div className={`${Styles.ArticleImg}`}>
+              <img className={`w-2`} src={`${props.coverUrl}`} />
+            </div>
+            {/* Card Info */}
+            <div className={`${Styles.CardInfo}`}>
+              {/* Author */}
+              <div className={`${Styles.autor}`}>
+                <img
+                  className={`rounded-circle border border-secondary ${Styles.profile}`}
+                  src={props.coverUrl}
+                />
+                <div className={`${Styles.autorName}`}>
+                  <div>{props.userId}</div>
+                  <div>{props.creationDate}</div>
+                </div>
               </div>
-              {/* Card Info */}
-              <div className={`${Styles.CardInfo}`}>
-                {/* Author */}
-                <div className={`${Styles.autor}`}>
-                  <img
-                    className={`rounded-circle border border-secondary ${Styles.profile}`}
-                    src={authorImg}
-                  />
-                  <div className={`${Styles.autorName}`}>
-                    <div>{author}</div>
-                    <div>{date}</div>
-                  </div>
+              {/* Title */}
+              <div>
+                <h2 className={`${Styles.cardTitle}`}>
+                  <a href={`./detailPost/${props.id}`}>{props.title}</a>
+                </h2>
+              </div>
+              {/* Tags */}
+              <div className={`${Styles.tags}`}>
+                <a href="#">
+                  <span>#</span>beginners
+                </a>
+                <a href="#">
+                  <span>#</span>productivity
+                </a>
+                <a href="#">
+                  <span>#</span>javascript
+                </a>
+                <a href="#">
+                  <span>#</span>webdev
+                </a>
+              </div>
+              {/* Reactions */}
+              <div className={`${Styles.reactions}`}>
+                <div className={`${Styles.reactLeft}`}>
+                  <a href="#">
+                    <img src={`${imgReactions}`} />
+                    <span>{props.likes}</span>
+                    <span className="react-text"> &nbsp;reactions</span>
+                  </a>
+                  <a href="#">
+                    <img src={imgComments} />
+                    <span>{props.likes}</span>
+                    <span className="react-text"> &nbsp;comments</span>
+                  </a>
                 </div>
-                {/* Title */}
-                <div>
-                  <h2 className={`${Styles.cardTitle}`}>
-                    <a href="./">{title}</a>
-                  </h2>
+                <div className="react-right">
+                  <span>{props.duration} min read</span>
+                  <button>Save</button>
                 </div>
-                {/* Tags */}
-                <div className={`${Styles.tags}`}>
-                  <a href="#">
-                    <span>#</span>beginners
-                  </a>
-                  <a href="#">
-                    <span>#</span>productivity
-                  </a>
-                  <a href="#">
-                    <span>#</span>javascript
-                  </a>
-                  <a href="#">
-                    <span>#</span>webdev
-                  </a>
-                </div>
-                {/* Reactions */}
-                <div className={`${Styles.reactions}`}>
-                  <div className={`${Styles.reactLeft}`}>
-                    <a href="#">
-                      <img src={`${imgReactions}`} />
-                      <span>{reactions}</span>
-                      <span className="react-text"> &nbsp;reactions</span>
-                    </a>
-                    <a href="#">
-                      <img src={imgComments} />
-                      <span>{comments}</span>
-                      <span className="react-text"> &nbsp;comments</span>
-                    </a>
-                  </div>
-                  <div className="react-right">
-                    <span>{read} min read</span>
-                    <button>Save</button>
-                  </div>
-                </div>{" "}
-                {/* End Reactions */}
               </div>{" "}
-              {/* End Card Info */}
-            </li>
-          )
-        )}
-      </React.Fragment>
-    ));
-
-  return <div>{postsUI}</div>;
+              {/* End Reactions */}
+            </div>{" "}
+            {/* End Card Info */}
+          </li>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
-
-export default Post;
--- src/components/Content/SidePosts/index.jsx
+++ src/components/Content/SidePosts/index.jsx
@@ -34,95 +34,41 @@ import React from "react";
 import { useHistory } from "react-router";
 
 import Post from "./Post";
-import Styles1 from "./index.module.css";
-
-import Styles from "./Post.module.css";
-
-// Rutas imagenes
-import imgArticleImg from "../../../images/photo-main.jpeg";
-
-import profile1 from "../../../images/profiles/user2.webp";
-import profile2 from "../../../images/profiles/user3.webp";
-import profile3 from "../../../images/profiles/user4.webp";
-
-import imgReactions from "../../../images/reaction-heart.svg";
-import imgComments from "../../../images/reaction-comment.svg";
+import Styles from "./index.module.css";
 
 export default function Content(props) {
   const history = useHistory();
   console.log(props.data);
-  const buildPosts = ([
-    key,
-    { content, coverUrl, creationDate, duration, likes, tags, title, userId },
-  ]) => {
+  const buildPosts = (
+    [
+      key,
+      {
+        _id: id,
+        content,
+        coverUrl,
+        creationDate,
+        duration,
+        likes,
+        tags,
+        title,
+        userId,
+      },
+    ],
+    index
+  ) => {
     return (
-      <React.Fragment>
-        <div key={key} className={`card ${Styles.AppCard}`}>
-          <div className="card-body">
-            <li className={`card ${Styles.CardContainer}`}>
-              {/* Imagen del Post */}
-              <div className={`${Styles.ArticleImg}`}>
-                <img className={`w-2`} src={`${coverUrl}`} />
-              </div>
-              {/* Card Info */}
-              <div className={`${Styles.CardInfo}`}>
-                {/* Author */}
-                <div className={`${Styles.autor}`}>
-                  <img
-                    className={`rounded-circle border border-secondary ${Styles.profile}`}
-                    src={coverUrl}
-                  />
-                  <div className={`${Styles.autorName}`}>
-                    <div>{userId}</div>
-                    <div>{creationDate}</div>
-                  </div>
-                </div>
-                {/* Title */}
-                <div>
-                  <h2 className={`${Styles.cardTitle}`}>
-                    <a href="./">{title}</a>
-                  </h2>
-                </div>
-                {/* Tags */}
-                <div className={`${Styles.tags}`}>
-                  <a href="#">
-                    <span>#</span>beginners
-                  </a>
-                  <a href="#">
-                    <span>#</span>productivity
-                  </a>
-                  <a href="#">
-                    <span>#</span>javascript
-                  </a>
-                  <a href="#">
-                    <span>#</span>webdev
-                  </a>
-                </div>
-                {/* Reactions */}
-                <div className={`${Styles.reactions}`}>
-                  <div className={`${Styles.reactLeft}`}>
-                    <a href="#">
-                      <img src={`${imgReactions}`} />
-                      <span>{likes}</span>
-                      <span className="react-text"> &nbsp;reactions</span>
-                    </a>
-                    <a href="#">
-                      <img src={imgComments} />
-                      <span>{likes}</span>
-                      <span className="react-text"> &nbsp;comments</span>
-                    </a>
-                  </div>
-                  <div className="react-right">
-                    <span>{duration} min read</span>
-                    <button>Save</button>
-                  </div>
-                </div>{" "}
-                {/* End Reactions */}
-              </div>{" "}
-              {/* End Card Info */}
-            </li>
-          </div>
-        </div>
+      <React.Fragment key={index}>
+        <Post
+          id={id}
+          content={content}
+          coverUrl={coverUrl}
+          creationDate={creationDate}
+          duration={duration}
+          likes={likes}
+          tags={tags}
+          title={title}
+          userId={userId}
+        />
       </React.Fragment>
     );
   };
