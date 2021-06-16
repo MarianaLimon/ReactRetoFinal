50bf6a215af861a9157d63612da5dbf204b41b21
jedmundo-rm <jedmundo.rm@gmail.com>
2021-06-16 01:12:02 -0500

    ajuste filter mobile

--- src/components/Content/SidePosts/DateFilter/index.jsx
+++ src/components/Content/SidePosts/DateFilter/index.jsx
@@ -5,8 +5,8 @@ import "./style.css";
 class DateFilter extends React.Component {
   render() {
     return (
-      <nav className="d-flex justify-content-between pb-2">
-        <h3>Post</h3>
+      <nav className="d-flex justify-content-between align-items-center pb-2">
+        <h3><b>Posts</b></h3>
         <ul className="filter d-none d-lg-block">
           <li className="active">Feed</li>
           <li>Week</li>
